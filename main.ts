abe89c43078f3228d27e0e485d7691aab3ac06aa
geekaroo77 <32786941+geekaroo77@users.noreply.github.com>
2021-04-03 15:02:48 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,47 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    fish.setPosition(randint(0, 160), randint(0, 120))
+    info.startCountdown(3)
+})
+let fish: Sprite = null
+let shark = sprites.create(img`
+    .............ccfff..............
+    ............cddbbf..............
+    ...........cddbbf...............
+    ..........fccbbcf............ccc
+    ....ffffffccccccff.........ccbbc
+    ..ffbbbbbbbbbbbbbcfff.....cdbbc.
+    ffbbbbbbbbbcbcbbbbcccff..cddbbf.
+    fbcbbbbbffbbcbcbbbcccccfffdbbf..
+    fbbb1111ff1bcbcbbbcccccccbbbcf..
+    .fb11111111bbbbbbcccccccccbccf..
+    ..fccc33cc11bbbbccccccccfffbbcf.
+    ...fc131c111bbbcccccbdbc...fbbf.
+    ....f33c111cbbbfdddddcc.....fbbf
+    .....ff1111fbdbbfddcc........fff
+    .......cccccfbdbbfc.............
+    .............fffff..............
+    `, SpriteKind.Player)
+controller.moveSprite(shark)
+shark.setStayInScreen(true)
+scene.setBackgroundColor(8)
+fish = sprites.create(img`
+    . . . . . . . . c c c c . . . . 
+    . . . . . . c c d d d d c . . . 
+    . . . . . c c c c c c d c . . . 
+    . . . . c c 4 4 4 4 d c c . c c 
+    . . . c 4 d 4 4 4 4 4 1 c c 4 c 
+    . . c 4 4 4 1 4 4 4 4 d 1 c 4 f 
+    . c 4 4 4 4 1 4 4 4 4 4 1 4 4 f 
+    f 4 4 4 4 4 1 1 c f 4 4 1 f 4 f 
+    f 4 4 4 f 4 1 c 4 f 4 4 1 f 4 f 
+    f 4 4 4 4 4 1 4 4 f 4 4 d f f f 
+    . f 4 4 4 4 1 c c 4 4 d f f . . 
+    . . f f 4 d 4 4 4 4 4 c f . . . 
+    . . . . f f 4 4 4 4 c d b c . . 
+    . . . . . . f f f f d d d c . . 
+    . . . . . . . . . . c c c . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Food)
+fish.setPosition(randint(0, 160), randint(0, 120))
+info.startCountdown(3)
